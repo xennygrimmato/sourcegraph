xennygrimmato/sourcegraph

commit a24c1aa78cf4a2b6c711ab88c4266187ab29127a

server/internal/store/fs: concurrent Repos.setGitConfig

server/internal/store/fs/repos_git_config.go
@@ -37,13 +37,19 @@ func (s *Repos) setGitConfig(ctx context.Context, dir, name, value string) error
 		return err
 	}
 
-	cmd := exec.Command("git", "config", name, value)
-	cmd.Dir = dir
-	out, err := cmd.CombinedOutput()
-	if err != nil {
-		return fmt.Errorf("set git config %q %q failed with %s\n%s", name, value, err, out)
+	var (
+		err error
+		out []byte
+	)
+	for attempt := 0; attempt < 3; attempt++ {
+		cmd := exec.Command("git", "config", name, value)
+		cmd.Dir = dir
+		out, err = cmd.CombinedOutput()
+		if err != nil {
+			fmt.Printf("set git config %q %q failed with %s\n%s", name, value, err, out)
+		}
 	}
-	return nil
+	return err
 }
 
 func (s *Repos) getGitConfig(ctx context.Context, fs vfs.FileSystem, dir string) (*repoGitConfig, error) {

commit 6a9fd8a737bcfef9ee5829ef09a75bbdbb2e007a

server/internal/store/fs: fix bug with setGitConfig
introduced in a24c1aa78cf4a2b6c711ab88c4266187ab29127a

server/internal/store/fs/repos_git_config.go
@@ -48,6 +48,7 @@ func (s *Repos) setGitConfig(ctx context.Context, dir, name, value string) error
 		if err != nil {
 			fmt.Printf("set git config %q %q failed with %s\n%s", name, value, err, out)
 		}
+		return nil
 	}
 	return err
 }