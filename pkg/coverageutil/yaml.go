230e01ba60325960b72594bfc304132a68515545
Farhan Attamimi <farhan.attamimi@gmail.com>
2016-06-01 20:17:23 -0700

    cleanup, add comments

--- pkg/coverageutil/yaml.go
+++ pkg/coverageutil/yaml.go
@@ -6,13 +6,19 @@ import (
 )
 
 type yamlTokenizer struct {
+	// string values of tokens in the tokenizer
 	values     []string
 	startBytes []int
 	line       []int
-	pointer    int
+	// pointer points to the current index in the tokenizer
+	// when we traverse the tokenizer to get each token
+	pointer int
 }
 
-type T struct {
+// TokenInfo struct is used to hold line, column, and text
+// for all tokens in a file
+type TokenInfo struct {
+	// value is the text value of the tokens in the token info list
 	value  []string
 	line   []int
 	column []int
@@ -20,23 +26,32 @@ type T struct {
 
 // Initializes text scanner that extracts only idents
 func (s *yamlTokenizer) Init(src []byte) {
+	// construct a new parser
 	p := yaml.NewParser(src)
+	// node is the tree of tokens returned by the parser
 	node := p.Parse()
 	var nodeList []*yaml.Node
+	// tokenList is a list of nodes representing tokens
+	// created by traversing the tree of nodes
 	tokenList := yaml.Explore(node, nodeList)
-	out := &T{}
-	getLineAndColumn(tokenList, string(src), out)
+
+	out := &TokenInfo{}
+	// we fill an out struct with the token value and matching line and column
+	fillLineAndColumn(tokenList, out)
+	fileString := string(src)
+	// traverse the value field of the out struct to get
+	// the starting byte offsets for each token and store the
+	// relevant information for each token in the tokenizer
 	for i, _ := range out.value {
-		start, _, value := findOffsets(string(src), out.line[i], out.column[i], out.value[i])
+		start, value := findOffsets(fileString, out.line[i], out.column[i], out.value[i])
 		s.line = append(s.line, out.line[i])
 		s.values = append(s.values, value)
 		s.startBytes = append(s.startBytes, start)
 	}
-
 	s.pointer = 0
 }
 
-// Next returns idents that are not Java keywords
+// Next returns YAML idents
 func (s *yamlTokenizer) Next() *Token {
 	if s.pointer >= len(s.values) {
 		return nil
@@ -60,27 +75,24 @@ func init() {
 	newExtensionBasedLookup("YAML", []string{".yml"}, factory)
 }
 
-func getLineAndColumn(tokenList []*yaml.Node, fileString string, out *T) {
+func fillLineAndColumn(tokenList []*yaml.Node, out *TokenInfo) {
 	for _, token := range tokenList {
 		out.value = append(out.value, token.Value)
 		out.line = append(out.line, token.Line)
 		out.column = append(out.column, token.Column)
-		// a, b := findOffsets(data, token.Line, token.Column, token.Value)
-		// fmt.Println("start: ", a, "End: ", b)
-		getLineAndColumn(token.Children, fileString, out)
+		fillLineAndColumn(token.Children, out)
 	}
 }
 
-func findOffsets(fileText string, line, column int, token string) (start, end int, value string) {
+func findOffsets(fileText string, line, column int, token string) (start int, value string) {
 
 	// we count our current line and column position.
 	currentCol := 0
 	currentLine := 0
-	for offset, ch := range fileText {
-		// see if we found where we wanted to go to.
+	for offset, ch := range byte[](fileText) {
+		fmt.Println(ch)
 		if currentLine == line && currentCol == column {
-			end = offset + len([]byte(token))
-			return offset, end, token
+			return offset, token
 		}
 
 		// line break - increment the line counter and reset the column.
@@ -91,5 +103,5 @@ func findOffsets(fileText string, line, column int, token string) (start, end in
 			currentCol++
 		}
 	}
-	return -1, -1, token // not found.
+	return -1, token // not found.
 }
