8d337ec21c78096094022ecc0dbe1f25b0ed6ed0
Mate Kovacs <mkovaxx@gmail.com>
2016-06-03 13:19:50 -0700

    coverage: hook up bash lexer

--- pkg/coverageutil/bash.go
+++ pkg/coverageutil/bash.go
@@ -3,32 +3,44 @@ package coverageutil
 import (
 	"bytes"
 
-	"text/scanner"
+	"github.com/mkovacs/bash/scanner"
 )
 
-// bashTokenizer produces tokens from a Bash script
-type bashTokenizer struct {
-	scanner *scanner.Scanner
+// bashIdentScanner extracts identifiers from a Bash script
+type bashIdentScanner struct {
+	is *scanner.Scanner
 }
 
 // Initializes text scanner that extracts only idents
-func (s *bashTokenizer) Init(src []byte) {
-	s.scanner = &scanner.Scanner{}
-	s.scanner.Error = func(s *scanner.Scanner, msg string) {}
-	s.scanner.Init(bytes.NewReader(src))
+func (s *bashIdentScanner) Init(src []byte) {
+	s.is = &scanner.Scanner{}
+	s.is.Init(bytes.NewReader(src))
 }
 
-func (s *bashTokenizer) Done() {
+func (s *bashIdentScanner) Done() {
 }
 
-// Next returns idents that are not bash keywords
-func (s *bashTokenizer) Next() *Token {
-	return nil
+// Next returns the next identifier
+func (s *bashIdentScanner) Next() *Token {
+	for {
+		tok, err := s.is.Scan()
+		if err != nil {
+			return nil
+		}
+		switch {
+		case tok == scanner.EOF:
+			return nil
+		case tok == scanner.Ident:
+			text := s.is.TokenText()
+			p := s.is.Pos()
+			return &Token{uint32(p.Offset - len(text)), p.Line, text}
+		}
+	}
 }
 
 func init() {
 	var factory = func() Tokenizer {
-		return &bashTokenizer{}
+		return &bashIdentScanner{}
 	}
 	newExtensionBasedLookup("Bash", []string{".sh"}, factory)
 }
