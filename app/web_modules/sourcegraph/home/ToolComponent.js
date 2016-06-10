c4df53fa5af2a0675932d617de814798bf97fb01
Matt Sprague <matt@sourcegraph.com>
2016-06-09 23:27:13 -0700

    basic form for sfye

--- app/web_modules/sourcegraph/home/ToolComponent.js
+++ app/web_modules/sourcegraph/home/ToolComponent.js
@@ -5,6 +5,10 @@ import base from "sourcegraph/components/styles/_base.css";
 import {Heading, Panel, Button} from "sourcegraph/components";
 import {CloseIcon} from "sourcegraph/components/Icons";
 import Modal from "sourcegraph/components/Modal";
+import Selector from "./Selector";
+import {TriangleRightIcon, TriangleDownIcon} from "sourcegraph/components/Icons";
+import HomeBackend from "sourcegraph/home/HomeBackend";
+import Dispatcher from "sourcegraph/Dispatcher";
 
 class ToolComponent extends React.Component {
 
@@ -19,12 +23,60 @@ class ToolComponent extends React.Component {
 		router: React.PropTypes.object.isRequired,
 	};
 
+	constructor(props) {
+		super(props);
+		this.state = {
+			visibility: false,
+			submitted: false,
+		};
+
+		this.languages = {
+			java: "Java",
+			python: "Python",
+			php: "PHP",
+			csharp: "C#",
+			javascript: "Javascript",
+			cplusplus: "C++",
+			c: "C",
+			objectivec: "Objective C",
+			ruby: "Ruby",
+			swift: "Swift",
+		};
+
+		this.editors = {
+			eclipse: "Eclipse",
+			emacs: "Emacs",
+			atom: "Atom",
+			vscode: "Visual Studio Code",
+			pycharm: "PyCharm",
+			rubymine: "RubyMine",
+			IntelliJ: "IntelliJ",
+		};
+	}
+
 	_dismissModal() {
 		this.context.eventLogger.logEvent("ToolBackButtonClicked", {toolType: this.props.location.query.tool});
 		this.context.router.replace({...this.props.location, query: ""});
 	}
+	_toggleView() {
+		this.setState({visibility: !this.state.visibility});
+		console.log(this.state.visibility);
+	}
+	_getVisibility() {
+		return this.state.visibility;
+	}
+	_sendForm(ev) {
+		this.setState({submitted: true});
+		let data = {
+			email: ev.currentTarget[0]['value'],
+			editor: ev.currentTarget[1]['value'],
+			lanuage: ev.currentTarget[2]['value'],
+		};
+		Dispatcher.Backends.dispatch(new HomeBackend.FormSignup("form_id", data));
+	}
 
 	render() {
+		// flex-container ${this._getVisibility() ? "visible" : "invisible"}
 		return (
 			<Modal onDismiss={this._dismissModal.bind(this)}>
 					<div styleName="tool-item">
@@ -46,6 +98,31 @@ class ToolComponent extends React.Component {
 								</div>
 							</div>
 							<div styleName="button-container">{this.props.supportedTool.primaryButton}</div>
+							<div>
+								<div>{this._getVisibility()?<TriangleDownIcon />:<TriangleRightIcon />}<a onClick={() => this._toggleView()}>Don't see the editor that you use? Let us know what we should work on next!</a>
+								</div>
+								<div styleName={this._getVisibility() ? "visible" : "invisible"}>
+								{!this.state.submitted?
+									<form onSubmit={this._sendForm}>
+										<div styleName="question-container">
+											<span>Email</span>
+											<input type="text" name="email" />
+											<div>
+												<span>Preferred editor:</span>
+												<Selector mapping={this.editors} />
+											</div>
+											<div>
+												<span>Programming langauge:</span>
+												<Selector mapping={this.languages} />
+											</div>
+										</div>
+									<Button>Let me know when Sourcegraph is ready for my editor!</Button>
+								</form> :
+								<span>Thanks for your feedback!</span>
+								}
+							</div>
+							</div>
+
 							{this.props.supportedTool.secondaryButton}
 							{this.props.supportedTool.gif && <div styleName="tool-gif-container">
 								<img styleName="tool-gif" src={`${this.context.siteConfig.assetsRoot}${this.props.supportedTool.gif}`}></img>
@@ -57,4 +134,4 @@ class ToolComponent extends React.Component {
 	}
 }
 
-export default CSSModules(ToolComponent, styles);
+export default CSSModules(ToolComponent, styles, {allowMultiple: true});
