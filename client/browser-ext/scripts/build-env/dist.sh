6eafb9a16031708bdccf40258455f1347b485a9b
Matt Sprague <matt@sourcegraph.com>
2016-06-24 17:33:47 -0700

    browser extension zips have hash and version number in filename

--- client/browser-ext/scripts/build-env/dist.sh
+++ client/browser-ext/scripts/build-env/dist.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+set -e # quit script if anything fails
 cd /browser-ext
 rm -rf node_modules
 rm -rf build/
@@ -7,5 +8,7 @@ rm -rf dev/
 npm install
 npm run build
 cd /browser-ext/build
-zip -r /browser-ext/firefox-sourcegraph-dist.xpi *
-zip -r /browser-ext/chrome-sourcegraph-dist.zip *
+export version_string=`grep \"version\" manifest.json`
+version_string=${version_string:14:6}
+zip -r /browser-ext/firefox-sourcegraph-dist-${GITHUB_HASH:0:8}-${version_string}.xpi *
+zip -r /browser-ext/chrome-sourcegraph-dist-${GITHUB_HASH:0:8}-${version_string}.zip *
