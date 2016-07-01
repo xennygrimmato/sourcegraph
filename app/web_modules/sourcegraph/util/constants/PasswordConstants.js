bf5616187f75bf014f15c1a1b6b207a31a4d7930
Geoffrey Gilmore <ggilmore@mit.edu>
2016-07-01 15:44:41 -0700

    added escaping to \d

--- app/web_modules/sourcegraph/util/constants/PasswordConstants.js
+++ app/web_modules/sourcegraph/util/constants/PasswordConstants.js
@@ -1,7 +1,7 @@
 // Password Constants
 
-// PASSWORD_PATTERN is the regex that represents the allowed passwords for the signup and reset password pages.
-export const PASSWORD_PATTERN = "^(?!.*(.)\\1{2})(?=.*[^A-Za-z\d])(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,128}$";
+// PASSWORD_PATTERN is the regex that represents the allowed passwords for the join and reset password pages.
+export const PASSWORD_PATTERN = "^(?!.*(.)\\1{2})(?=.*[^A-Za-z\\d])(?=.*\\d)(?=.*[a-z])(?=.*[A-Z]).{10,128}$";
 
 // PASSWORD_DESCRIPTION is the "english" description of the password requirements enforced by PASSWORD_PATTERN.
 export const PASSWORD_DESCRIPTION = `Your password should be between 10 and 128 characters (with no more than two identical characters in a row), and should include at least: 
