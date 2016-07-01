ef211eca94d3fe4d80dfdafbf1023fe43195c731
Geoffrey Gilmore <ggilmore@mit.edu>
2016-07-01 15:21:14 -0700

    added note about repeated characters

--- app/web_modules/sourcegraph/util/constants/PasswordConstants.js
+++ app/web_modules/sourcegraph/util/constants/PasswordConstants.js
@@ -4,7 +4,7 @@
 export const PASSWORD_PATTERN = "^(?!.*(.)\\1{2})(?=.*[^A-Za-z\d])(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{10,128}$";
 
 // PASSWORD_DESCRIPTION is the "english" description of the password requirements enforced by PASSWORD_PATTERN.
-export const PASSWORD_DESCRIPTION = `Your password should be between 10 and 128 characters, and should include at least: 
+export const PASSWORD_DESCRIPTION = `Your password should be between 10 and 128 characters (with no more than two identical characters in a row), and should include at least: 
 - 1 uppercase character 
 - 1 lowercase character
 - 1 digit
