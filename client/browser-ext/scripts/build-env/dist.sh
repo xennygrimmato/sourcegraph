2bde980afa5514485053533b18ba5167f8afab34
Caroline Frost <caroline@sourcegraph.com>
2016-07-05 15:31:56 -0700

    undoing other com

--- client/browser-ext/scripts/build-env/dist.sh
+++ client/browser-ext/scripts/build-env/dist.sh
@@ -8,5 +8,5 @@ rm -rf dev/
 npm install
 npm run build
 cd /browser-ext/build
--zip -r /browser-ext/firefox-sourcegraph-dist.xpi *
--zip -r /browser-ext/chrome-sourcegraph-dist.zip *
+zip -r /browser-ext/firefox-sourcegraph-dist.xpi *
+zip -r /browser-ext/chrome-sourcegraph-dist.zip *
