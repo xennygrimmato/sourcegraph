af139de4f9b39c9f889353ad14c49601a1b878ca
Quinn Slack <qslack@qslack.com>
2016-07-06 04:41:41 -0700

    app: do not scroll when the pathname remains the same

--- app/web_modules/sourcegraph/app/routerScrollBehavior.js
+++ app/web_modules/sourcegraph/app/routerScrollBehavior.js
@@ -8,9 +8,15 @@ import type {RouterState} from "react-router";
 // achieve the desirable behavior of the window not scrolling when
 // the user clicks on a def (and goes to a def route) on the blob page,
 // or performs an incremental search that updates the URL.
+//
+// It also handles not scrolling when the pathname does not change.
 export function shouldUpdateScroll(prevRouterProps: ?RouterState, nextRouterProps: ?RouterState): bool {
 	if (!prevRouterProps) return true;
 	if (!nextRouterProps) return true;
+
+	const ploc = prevRouterProps.location;
+	const nloc = nextRouterProps.location;
+	if (ploc.pathname === nloc.pathname) return false;
 
 	const prevRoute = prevRouterProps.routes[prevRouterProps.routes.length - 1];
 	const nextRoute = nextRouterProps.routes[nextRouterProps.routes.length - 1];
