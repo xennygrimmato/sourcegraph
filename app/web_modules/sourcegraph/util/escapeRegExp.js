825250787dcd57c1cfe6e2ff051421982eaa4ac6
Geoffrey Gilmore <ggilmore@mit.edu>
2016-07-14 14:16:56 -0700

    added documentation to escapeRegExp.js

--- app/web_modules/sourcegraph/util/escapeRegExp.js
+++ app/web_modules/sourcegraph/util/escapeRegExp.js
@@ -1,3 +1,6 @@
+// This function escapes user input "str", and ensures that eveything in "str" is used
+// as a literal string when feeding the output to a RegExp constructor.
+// Taken from: https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 export default function(str) {
 	return str.replace(/[.*+?^${}()|[\]\\]/g, "\\$&");
 }
