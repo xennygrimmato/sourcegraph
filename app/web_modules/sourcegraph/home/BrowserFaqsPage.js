5c50962c81dec3024eab40599c346787684c3be9
carolinefrost <caroline@sourcegraph.com>
2016-07-15 09:17:07 -0700

    changes for ext

--- app/web_modules/sourcegraph/home/BrowserFaqsPage.js
+++ app/web_modules/sourcegraph/home/BrowserFaqsPage.js
@@ -44,27 +44,25 @@ class BrowserFaqsPage extends React.Component {
 					2,000,000,000+ nodes in the public code index (and growing). Sourcegraph currently supports Go and Java. Sourcegraph's code index powers the Sourcegraph browser Extension.</p>
 
 					<Heading level="3" underline="blue" className={styles.h5}>Can I use the extension anonymously?</Heading>
-					<p styleName="p">Sourcegraph indexes popular public repositories written in supported languages. Don't see a public repository you'd like indexed? Sign in @ Sourcegraph.com,
-					and the next time you visit that repository on GitHub, we'll start building it for you.</p>
+					<div styleName="p">Sourcegraph indexes popular public repositories written in supported languages. Don't see a public repository you'd like indexed? Sign in at Sourcegraph.com,
+					and the next time you visit that <a id="signin"/>repository on GitHub, we'll start building it for you.</div>
 
-					<a id="signin"/>
-					<Heading level="3" underline="blue" className={styles.h5}>How do I sign in to see my indexed code?</Heading>
-					<p styleName="p"> You must be signed in on Sourcegraph.com to access your indexed private repositories, or enable Sourcegraph to index your private repositories. Sign in or
-					sign up below with GitHub OAuth.</p>
+					<Heading level="3" underline="blue" className={styles.h5}>How come I can't see my private code on my private repositories?</Heading>
+					<div styleName="p"> You must be signed in on Sourcegraph.com to access your indexed private repositories. Sign in or
+					sign up below with GitHub OAuth.<a id="enable"/></div>
 					<div styleName="tc">
 					{!this.context.signedIn && <GitHubAuthButton returnTo="/about/browser-faqs#signin"> Sign up or sign in</GitHubAuthButton>}
 					</div>
 
-					<a id="enable"/>
 					<Heading level="3" underline="blue" className={styles.h5}>How do I use the browser extension with private repositories?</Heading>
-					<p styleName="p"> Sourcegraph must be enabled in order for your private repositories to be indexed. Authorize your repositories at <Link to="settings/repos">
+					<p styleName="p"> Sourcegraph indexes repositories that you have enabled. Enable your repositories at <Link to="settings/repos">
 					sourcegraph.com/settings/repos </Link>. </p>
 
 					<Heading level="3" underline="blue" className={styles.h5}>Where else is Sourcegraph?</Heading>
 					<p styleName="p"> Like the Sourcegraph browser extension? Sourcegraph can also be used on your desktop, in your editor. Try global semantic search on our homepage
 					at sourcegraph.com and check out more tools at <Link to="/tools"> sourcegraph.com/tools</Link>.</p>
 
-					<a id="buildfailure"/>
+					<a id="build"/>
 					<Heading level="3" underline="blue" className={styles.h5}>Troubleshooting</Heading>
 					<p styleName="p"> Sourcegraph runs static code analysis on your code. Properly indexing code requires some time, but if there are errors running the analysis, Sourcegraph does not index the code.
 					We don't support all project build (pants) types. If you'd like to report a bug, please send an email to <a href="mailto:support@sourcegraph.com"> support@sourcegraph.com</a>.</p>
