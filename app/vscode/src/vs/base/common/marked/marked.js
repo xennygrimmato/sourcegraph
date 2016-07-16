f531f6872e2c41f9b502360bf7a349b3717f79fe
Quinn Slack <qslack@qslack.com>
2016-07-16 14:09:39 -0700

    vscode(nomerge): use standard way of loading marked (markdown module)

--- app/vscode/src/vs/base/common/marked/marked.js
+++ app/vscode/src/vs/base/common/marked/marked.js
@@ -3,6 +3,15 @@
  *  Licensed under the MIT License. See License.txt in the project root for license information.
  *--------------------------------------------------------------------------------------------*/
 
+import * as marked_ from 'vs/base/common/marked/raw.marked';
+
+// HACK(sourcegraph): This is a workaround to make marked importable
+// as `import {marked} from ...` by existing code (so we don't need
+// to change those import statements in all imports).
+export const marked = marked_;
+
+
+/*
 require.config({
 	shim: {
 		'vs/base/common/marked/raw.marked': {
@@ -18,3 +27,4 @@ define(['./raw.marked'], function (marked) {
 		marked: marked
 	};
 });
+*/
